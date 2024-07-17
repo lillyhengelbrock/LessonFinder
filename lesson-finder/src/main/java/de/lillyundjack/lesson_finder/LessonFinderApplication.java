e55292d7b2af0ba7c95e4c5c93f41ac1ef1097b3
lillyhengelbrock <142220672+lillyhengelbrock@users.noreply.github.com>
2024-07-17 21:15:07 +0200

    Initial Commit

---
+++ lesson-finder/src/main/java/de/lillyundjack/lesson_finder/LessonFinderApplication.java
@@ -0,0 +1,13 @@
+package de.lillyundjack.lesson_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonFinderApplication.class, args);
+	}
+
+}
---
+++ lesson-finder/src/test/java/de/lillyundjack/lesson_finder/LessonFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lillyundjack.lesson_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
