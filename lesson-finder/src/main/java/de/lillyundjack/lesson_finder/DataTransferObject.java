33c590963df86788e60f1b07f6552ab788f92bf6
lillyhengelbrock <142220672+lillyhengelbrock@users.noreply.github.com>
2024-07-22 20:13:39 +0200

    Add DTO to filter lesson data

---
+++ lesson-finder/src/main/java/de/lillyundjack/lesson_finder/DataTransferObject.java
@@ -0,0 +1,20 @@
+package de.lillyundjack.lesson_finder;
+
+public class DataTransferObject {
+    private String title;
+
+    public DataTransferObject() {}
+
+    public DataTransferObject(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+}
